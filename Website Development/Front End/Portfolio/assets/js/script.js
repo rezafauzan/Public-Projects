79f4edb4d81de72092fa093a064272d368f96ba4
rezafauzan <rezafauzan593@gmail.com>
2024-06-20 07:46:48 +0700

    Responsivness: Add new breakpoint for extra extra small device screen (max-width: 320px); Navbar: Fix layout on mobile screen and extra extra small screen; Hero Section, About Section, Feature Section: Improve responsiveness from min screen width 1200px to max screen width 320px

--- Website Development/Front End/Portfolio/assets/js/script.js
+++ Website Development/Front End/Portfolio/assets/js/script.js
@@ -1,22 +1,22 @@
 const navbar = document.getElementsByTagName('nav')[0]
 const navbar_toggler = document.querySelector('nav .navbar-toggler')
-const navbar_item = document.querySelectorAll('nav .nav-list, nav .cta')
+const navbar_item = document.querySelector('nav .nav-list')
 
 navbar_toggler.addEventListener('click', () => {
-    navbar_item[0].classList.toggle('show')
-    navbar_item[1].classList.toggle('show')
+    navbar_item.classList.toggle('show')
+    navbar_toggler.classList.toggle('show')
 })
 
 function handleScroll() {
     // Check if the page has been scrolled beyond 50px from the top
     if (window.scrollY > 50) {
         // Add 'scrolled' class to navbar
-        navbar.classList.add('scrolled');
+        navbar.classList.add('scrolled')
     } else {
         // Remove 'scrolled' class from navbar
-        navbar.classList.remove('scrolled');
+        navbar.classList.remove('scrolled')
     }
 }
 
 // Listen for the scroll event and call handleScroll function
-window.addEventListener('scroll', handleScroll);
+window.addEventListener('scroll', handleScroll)
